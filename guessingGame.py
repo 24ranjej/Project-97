e340479c174ed1c0c90abf4c92990bf7308aa225
24ranjej <69919771+24ranjej@users.noreply.github.com>
2021-09-18 16:32:45 -0500

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,13 @@
+print("number guessing game")
+print("guess a number between 1 and 9")
+
+number: 4
+
+while chances < 5:
+
+if guess == number: 
+    print("Congradulations YOU WON!!!")
+    break
+
+if not chance < 5:
+    print("YOU LOSE!!! The number was", number)
